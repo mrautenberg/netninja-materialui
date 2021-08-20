f77752073afcff86d9af504c4fc9b03fd73fb62e
Max Rautenberg <max.rautenberg89@gmail.com>
2021-08-20 14:52:23 +0200

    project done

---
+++ README.md
@@ -0,0 +1,1 @@
+### Netninja Material UI project: https://www.youtube.com/watch?v=gEbSx5CCgSc
---
+++ src/components/Layout.js
@@ -0,0 +1,123 @@
+import { makeStyles } from "@material-ui/core";
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { AddCircleOutlined, SubjectOutlined } from "@material-ui/icons";
+import { useHistory, useLocation } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Avatar from "@material-ui/core/Avatar";
+
+import { format } from "date-fns";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      // base spacing multiplied with 3
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    root: {
+      display: "flex",
+    },
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appbar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    // get classes associated with toolbar
+    toolbar: theme.mixins.toolbar,
+    date: {
+      flexGrow: 1,
+    },
+    avatar: {
+      marginLeft: theme.spacing(2),
+    },
+  };
+});
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+
+  const menuItems = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Create Note",
+      icon: <AddCircleOutlined color="secondary" />,
+      path: "/create",
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      {/* app bar */}
+      <AppBar className={classes.appbar} elevation={0}>
+        <ToolBar>
+          <Typography className={classes.date}>
+            Today is the {format(new Date(), "do MMMM Y")}
+          </Typography>
+          <Typography>Mario</Typography>
+          <Avatar src="/mario-av.png" className={classes.avatar} />
+        </ToolBar>
+      </AppBar>
+
+      {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>
+            Ninja Notes
+          </Typography>
+        </div>
+
+        {/* list / links */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem
+              button
+              key={item.text}
+              onClick={() => history.push(item.path)}
+              className={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
+    </div>
+  );
+}
